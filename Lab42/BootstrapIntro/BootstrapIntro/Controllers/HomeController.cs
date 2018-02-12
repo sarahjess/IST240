fd6a3212a547c41c55369b383906ad6d766ad206
sarahjess <35705297+sarahjess@users.noreply.github.com>
2018-02-12 16:56:02 -0500

    Lab 4.2 Added

---
+++ Lab42/BootstrapIntro/BootstrapIntro/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace BootstrapIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Workflow Reimagined";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "NHTI - SAJ Enterprises";
+
+            return View();
+        }
+
+        public ActionResult CalcApp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CalcApp(string valueA, string valueB, string radSelect) //Under Construction
+        {
+            Decimal total = 0;
+            var totalMsg = "";
+
+
+            switch (radSelect)
+            {
+                case "add":
+                    total = valueA.AsDecimal() + valueB.AsDecimal();
+                    break;
+
+                case "subtract":
+                    total = valueA.AsDecimal() - valueB.AsDecimal();
+                    break;
+
+                case "multiply":
+                    total = valueA.AsDecimal() * valueB.AsDecimal();
+                    break;
+
+                case "divide":
+                    total = valueA.AsDecimal() / valueB.AsDecimal();
+                    break;
+
+            }
+
+            //Display Result
+            totalMsg = "Result: " + total;
+            
+
+            return View("CalcApp", (object)totalMsg);
+        }
+    }
+}
---
+++ Lab42/BootstrapIntro/BootstrapIntro/obj/Debug/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BootstrapIntro.MvcApplication" Language="C#" %>
